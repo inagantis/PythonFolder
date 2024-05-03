d2b17dbcdff43eff3ab73896058b9ddbc410cf32
Srikanth Inaganti <inaganti.srikanth@gmail.com>
2024-05-03 13:34:34 +0530

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+first_name= 'srikanth'
+last_name='inaganti'
+print('hello ' + first_name + ' '+last_name)
+sentence = 'hello ' + first_name + ' '+ last_name
+print(sentence.upper())
+print(sentence.capitalize())
+print(sentence.lower())
